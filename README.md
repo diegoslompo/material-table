b8c175208188cc1a5ce880972b1a3de0d594a0eb
unknown <diegoslompo@gmail.com>
2019-11-18 00:11:56 -0300

    Feat Material Design Google

--- README.md
+++ README.md
@@ -1,14 +1,28 @@
+# Material Table
 
-## TODO
- - Test mocha, jest
- - style
- - animation
- - more infos (modal or acordion)
- - proptypes
- - routes
+Lista em React e click dinamico para lista de tarefas referente ao usuario selecionado
 
-## DONE
- - List
- - Redux logic
- - Logger
- - component 
+Requirements:
+* &#x2705; Lista Usuarios
+* &#x2705; Material Google
+
+Aditional:
+* &#x2705; Redux
+* &#x2705; Logger
+* &#x2705; Lista Tarefas por Usuarios
+* &#x2705; Style Component
+* &#x2705; Acordion Lista
+
+
+## Getting Started
+
+To get started app:
+
+* Clone the project
+* install all project dependencies with `npm install`
+* start the development server with `npm start`
+
+
+## Contributing
+
+Please read [CONTRIBUTING.md](CONTRIBUTING.md) for details on our code of conduct, and the process for submitting pull requests to us.
--- src/components/TableList.js
+++ src/components/TableList.js
@@ -1,44 +1,83 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import styled, { css }from 'styled-components'
+import List from '@material-ui/core/List'
+import ListItem from '@material-ui/core/ListItem'
+import ListItemAvatar from '@material-ui/core/ListItemAvatar'
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction'
+import ListItemText from '@material-ui/core/ListItemText'
+import Avatar from '@material-ui/core/Avatar'
+import IconButton from '@material-ui/core/IconButton'
+import AccountCircleIcon from '@material-ui/icons/AccountCircle'
+import AddIcon from '@material-ui/icons/Add'
+import Table from '@material-ui/core/Table'
+import TableBody from '@material-ui/core/TableBody'
+import TableCell from '@material-ui/core/TableCell'
+import TableRow from '@material-ui/core/TableRow'
+import Paper from '@material-ui/core/Paper'
+import Chip from '@material-ui/core/Chip'
 
 class TableList extends Component {
 
-  state = { open: false, userId: '' };
+  state = { open: false, userId: '' }
  
   toggle = (id) => {
-    this.setState(prevState => ({ open: !prevState.open, userId: id }));
-  };
+    this.setState(prevState => ({ open: true, userId: id }))
+  }
+
   openModal = (id) => { this.setState(() => ({ userId: id}))}
 
   render () {
 
     const {users, todos} = this.props
-    const { userId } = this.state
+    const { userId, open} = this.state
 
     return (
-      <List>
-        <ul className="App">
+      <ListTab>
+        <Users open={!open}>
           {users && (
             users.map( user => (
-              <li key={user.id}>
-                <span>{user.name}</span>
-                <button onClick={() => this.toggle(user.id)}>Open</button>
-              </li>
+              <UsersList key={user.id}>
+                <List>
+                  <ListItem>
+                    <ListItemAvatar>
+                      <Avatar>
+                        <AccountCircleIcon />
+                      </Avatar>
+                    </ListItemAvatar>
+                    <ListItemText
+                      primary={user.name}
+                    />
+                    <ListItemSecondaryAction onClick={() => this.toggle(user.id)}>
+                      <IconButton edge="end" aria-label="delete">
+                        <AddIcon />
+                      </IconButton>
+                    </ListItemSecondaryAction>
+                  </ListItem>
+                </List>
+              </UsersList>
             ))
           )}
-        </ul>
-        <Todos open={this.state.open}>
-          {todos && (
-              todos.map( todos => (todos.userId === userId) && (
-                <div key={todos.id}>
-                  <span>ID TASK: {todos.id}</span>
-                  <span>TITLE: {todos.title}</span>
-                  <span completed={todos.completed && 'true'}>{todos.completed ? 'completed': 'backlog'}</span>
-                </div>
-              ))
-            )}
+        </Users>
+        <Todos open={open}>
+            <Paper >
+              <Table aria-label="simple table">
+                <TableBody>
+                  {todos && (
+                    todos.map( todos => (todos.userId === userId) && (
+                      <TableRow key={todos.id}>
+                        <TableCell align="right">{todos.title}</TableCell>
+                        <TableCell align="right">
+                          <Chip color={todos.completed ? 'primary': 'secondary'} size="small" label={todos.completed ? 'completed': 'in backlog'}/>
+                          </TableCell>
+                      </TableRow>
+                    ))
+                  )}
+                </TableBody>
+              </Table>
+            </Paper>
         </Todos>
-      </List>
+      </ListTab>
     );
   }
 }
@@ -47,6 +86,36 @@ function mapStateToProps ({users, todos}) {
     return {users: users.data, todos: todos.data }
 }
 
+
+const ListTab = styled.div`
+  display: flex;
+  border: 1px solid #ccc;
+  margin: 40px;
+  font-family: sans-serif;
+`;
+const Users = styled.div`
+  width: 40%;
+  transition: 0.5s;
+
+  ${props => props.open && css`
+    width: 100%;
+  `}
+`;
+const UsersList = styled.li`
+  list-style: none;
+
+  &:nth-child(even) {
+    background: #fffcf9;
+  }
+`;
+const Todos = styled.div`
+  width: 0%;
+  transition: 0.3s;
+
+  ${props => props.open && css`
+    width: 60%;
+  `}
+`;
 
 
   
